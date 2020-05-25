9cc1817a5c741dc32289c23fa771613acf567db2
Yannis Kraemer <yannkra21@gmail.com>
2020-05-25 06:31:42 +0200

    Add finished schema.sql

--- KRAEMER_YANNIS/schema.sql
+++ KRAEMER_YANNIS/schema.sql
@@ -1,8 +1,17 @@
 -- Create database
-/* TODO: create database */
+CREATE DATABASE currencyChangerDB;
 
 -- Add tables
-/* TODO: add tables */
+CREATE TABLE currency_pair_price (
+    CurrencyStart VARCHAR(3),
+    CurrencyEnd VARCHAR(3),
+    Factor FLOAT
+);
 
 -- Fill in data
-/* TODO: add data */
+INSERT INTO currency_pair_price (CurrencyStart, CurrencyEnd, Factor) values ("USD", "CHF", 0.97);
+INSERT INTO currency_pair_price (CurrencyStart, CurrencyEnd, Factor) values ("CHF", "USD", 1.03);
+INSERT INTO currency_pair_price (CurrencyStart, CurrencyEnd, Factor) values ("CHF", "EUR", 0.94);
+INSERT INTO currency_pair_price (CurrencyStart, CurrencyEnd, Factor) values ("EUR", "CHF", 1.06);
+INSERT INTO currency_pair_price (CurrencyStart, CurrencyEnd, Factor) values ("EUR", "USD", 1.09);
+INSERT INTO currency_pair_price (CurrencyStart, CurrencyEnd, Factor) values ("USD", "EUR", 0.92);
